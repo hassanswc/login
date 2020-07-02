e167fadc8d6095c568e34fee40b5533b2973f4e3
Mahmoud <Mahmoud@MAHMOUD>
2020-07-02 15:30:33 -0400

    import code

---
+++ login/login.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+
+
+	int pin;
+	cout << "Enter PIN: ";
+	cin >> pin;
+
+	if (pin == 1234) {
+		cout << "You have entered!";
+	}
+	else {
+		cout << "Try again";
+	}
+
+
+
+
+
+}
+
